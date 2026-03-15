c0a291afa4af5e89f1f069086e135866116c2132
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-03-15 22:20:50 +0500

    data validation

--- pydantic_schema.py
+++ pydantic_schema.py
@@ -1,9 +1,11 @@
 from typing import List, Dict
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl
 
 class schema(BaseModel):
     name: str
     age: int
+    email: EmailStr
+    linkedin: AnyUrl
     smooker: bool
     weight: float
     height: float
@@ -13,13 +15,15 @@ class schema(BaseModel):
 def patient_detail(Schema: schema):
     print(Schema.name)
     print(Schema.age)
+    print(Schema.email)
     print(Schema.smooker)
     print(Schema.weight)
     print(Schema.height)
     print(Schema.allergies)
     print(Schema.content_details)
+    print(Schema.linkedin)
 
-detail = {'name': 'adnan', 'age': 23, 'smooker': True, 'weight': 88.3, 'height': 6.2, 'allergies': ['dust', 'foog'], 'content_details': {'email': 'saeedadnan151@gmail.com', 'phone': '3155682476', 'cnic': '4250118129795'}}
+detail = {'name': 'adnan', 'age': 23, 'smooker': True, 'email': 'saeedadnan151@gmail.com', 'weight': 88.3, 'height': 6.2, 'allergies': ['dust', 'foog'], 'content_details': {'phone': '3155682476', 'cnic': '4250118129795'}, 'linkedin': 'https://linkedin.com/adnansaeed'}
 
 pydantic_obj = schema(**detail)
 
