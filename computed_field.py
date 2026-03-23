e3101357255bfd23cb080d2aca6a239d1b97dad0
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-03-23 22:38:58 +0500

    height in meters

--- computed_field.py
+++ computed_field.py
@@ -28,7 +28,7 @@ def output(patient: patient_data):
     print(patient.contect_detail)
     print('BMI', patient.bmi_calculated)
 
-user_input = {'name': 'adnan saeed', 'age': 70, 'email': 'saeedadnan151@gmail.com', 'weight': 88.9, 'height': 6.2, 'allergies':['dust', 'sun'], 'contect_detail': {'emergency': '03155682476', 'name': 'brother'}, 'married': True}
+user_input = {'name': 'adnan saeed', 'age': 70, 'email': 'saeedadnan151@gmail.com', 'weight': 88.9, 'height': 2.0, 'allergies':['dust', 'sun'], 'contect_detail': {'emergency': '03155682476', 'name': 'brother'}, 'married': True}
 
 patient = patient_data(**user_input)
 
